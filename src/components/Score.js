f89a572c547e5c5f0c88d5624db71d06cffdaed8
jvitoralb <jvvalb@gmail.com>
2022-10-02 13:43:07 -0300

    Keep playing adding to count bug fix

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const Score = ({ toRender, resQuestions, finish }) => {
+const Score = ({ toRender, resQuestions }) => {
     const initialState = {
         totalQuestions: resQuestions.length,
         rightCount: 0
@@ -9,23 +9,10 @@ const Score = ({ toRender, resQuestions, finish }) => {
     const [score, setScore] = React.useState(initialState);
 
     /**
-     * Score is not resetting when Keep Playing is clicked
-     * BUT that can be good
-     * You should use this to just figure out how to increase total questions every time
-     * Keep Playing is clicked so the score keeps going
-     * or just reset everything and create a EndGame page
+     * Decide what to do with Score when Keep Playing is selected
     **/
 
-    /**
-     * Also there's a bug at last question
-     * if keep playing is clicked for some reason it adds one more to rightCount
-    **/
     React.useEffect(() => {
-        console.log('coucou')
-        // doesn't need this foreach
-        // just add every time questionRef.selected.includes(true)
-        // this happen because its a loop
-        // gonna add every time it runs and the conditional its met
         resQuestions.forEach(item => {
             if (item.resolve === 'resolve' && item.selected[1]) {
                 setScore(prev => ({
@@ -34,7 +21,7 @@ const Score = ({ toRender, resQuestions, finish }) => {
                 }));
             }
         });
-    }, [resQuestions, finish]);
+    }, [resQuestions]);
 
     const decodedCategory = decodeURIComponent(toRender.category);
 
