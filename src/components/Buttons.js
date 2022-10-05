2fa9047801906b9d7caadb610d79d32bf96473ca
jvitoralb <jvvalb@gmail.com>
2022-10-05 12:10:17 -0300

    add button component

---
+++ src/components/Buttons.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+
+const Buttons = ({ force, toRenderRef, resQuestions, util }) => {
+    const {
+        forceAnswer,
+        showAnswer,
+        finishGame,
+        updateRender
+    } = util;
+
+    const buttonDisplay = {
+        resolve: (clickFunc) => {
+            let lastIndex = resQuestions.at(-1) === toRenderRef;
+
+            if (clickFunc) {
+                return lastIndex ? finishGame : updateRender;
+            }
+            return lastIndex ? 'Keep Playing' : 'Next Question';
+        },
+        initial: (clickFunc) => {
+            if (clickFunc) {
+                return showAnswer;
+            }
+            return 'Show Answer';
+        }
+    }
+
+    return (
+        <React.Fragment>
+            {
+                force
+                ?
+                <div className='select-warning'>
+                    <p>You haven't selected any answer</p>
+                    <button
+                        onClick={() => forceAnswer(true)}
+                    >
+                        Show Anyways
+                    </button>
+                    <button
+                        onClick={() => forceAnswer(false)}
+                    >
+                        Cancel
+                    </button>
+                </div>
+                :
+                <button
+                    className='show-reset-btn'
+                    onClick={buttonDisplay[toRenderRef.resolve](true)}
+                >
+                    {buttonDisplay[toRenderRef.resolve](false)}
+                </button>
+            }
+        </React.Fragment>
+    );
+}
+
+export default Buttons;
