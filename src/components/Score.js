959acb5be41de4fab437fb6750a103d0ab7c9e16
jvitoralb <jvvalb@gmail.com>
2022-10-04 22:29:50 -0300

    changed useState to useReducer

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,43 +1,65 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 
-const Score = ({ toRender, resQuestions }) => {
+const Score = ({ toRender, resQuestions, finish }) => {
+    const reducer = (state, action) => {
+        switch(action.type) {
+            case 'increment':
+                return {
+                    ...state,
+                    rightCount: state.rightCount + 1
+                }
+            case 'play again':
+                return {
+                    ...state,
+                    totalQuestions: resQuestions.length + resQuestions.length
+                }
+            default:
+                throw new Error('something went wrong switch');
+        }
+    }
+
     const initialState = {
         totalQuestions: resQuestions.length,
         rightCount: 0
     }
-    const [score, setScore] = React.useState(initialState);
 
-    /**
-     * Decide what to do with Score when Keep Playing is selected
-    **/
+    const [score, dispatch] = useReducer(reducer, initialState);
+
+    React.useEffect(() => {
+        if (finish === 'play again') {
+            dispatch({type: 'play again'});
+        }
+    }, [finish]);
 
     React.useEffect(() => {
         resQuestions.forEach(item => {
             if (item.resolve === 'resolve' && item.selected[1]) {
-                setScore(prev => ({
-                    ...prev,
-                    rightCount: prev.rightCount += 1
-                }));
+                dispatch({type: 'increment'});
             }
         });
     }, [resQuestions]);
 
     const decodedCategory = decodeURIComponent(toRender.category);
 
-    const editedCategory = ((category) => {
-        return category.replace('Entertainment: ', '');
-    })(decodedCategory);
+    const editDifficulty = ((level) =>
+        level.slice(0, 1).toUpperCase() + level.slice(1)
+    )(toRender.difficulty);
+
+    const editCategory = ((category) =>
+        category.replace('Entertainment: ', '')
+    )(decodedCategory);
 
 // console.log(score)
     return (
         <React.Fragment>
             <div className='score'>
                 <p>Score {score.rightCount}/{score.totalQuestions}</p>
-                <p title={`Category ${decodedCategory}`}>{editedCategory}</p>
+                <p title={`Category ${decodedCategory}`}>{editCategory}</p>
+                <p title={`Difficulty ${editDifficulty}`}>{editDifficulty}</p>
             </div>
 
-            {/* Dev */}
+            {/* Dev 
             <button onClick={
                 () => setScore(initialState)}
             >
