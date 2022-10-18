8bd1b6299e526bcd05e425307b3af5b28b16449e
jvitoralb <jvvalb@gmail.com>
2022-10-17 22:00:30 -0300

    set up form

--- src/App.js
+++ src/App.js
@@ -5,46 +5,97 @@ import './components/Styling/index.css';
 
 
 const App = () => {
-    const [gameStatus, setGameStatus] = React.useState(false);
+    const [mainConfig, setMainConfig] = React.useState({
+        status: false,
+        category: {
+            trivia: [],
+            selected: 'any'
+        },
+        difficulty: {
+            all: ['any', 'easy', 'medium', 'hard'],
+            selected: 'any'
+        }
+    });
 
-    const start = () => setGameStatus(!gameStatus);
+    const start = () => setMainConfig(prevConfig => ({
+            ...prevConfig,
+            status: !prevConfig.status
+        })
+    );
 
-    // const devHomePage = (
-    //     <div className='home'>
-    //         <p>Let's Play a really cool Trivia Quiz</p>
-    //         <form>
-    //             <label>
-    //                 Pick a Category
-    //                 <select value={categories.category} onChange={selectCategory}>
-    //                     <option value={'Any'}>Any category</option>
-    //                     {/* Call API to get them all */}
-    //                 </select>
-    //             </label>
-    //             <label>
-    //                 Number of Questions
-    //                 <select value={categories.number} onChange={selectNumber}>
-    //                     <option value={5}>5</option>
-    //                     <option value={10}>10</option>
-    //                 </select>
-    //             </label>
-    //             <label>
-    //                 Difficulty
-    //                 <select value={categories.number} onChange={selectNumber}>
-    //                     <option value={5}>5</option>
-    //                     <option value={10}>10</option>
-    //                 </select>
-    //             </label>
-    //         </form>
-    //         <button onClick={start}>Start Playing</button>
-    //     </div>
-    // );
+    React.useEffect(() => {
+        const saveToGame = (data) => setMainConfig(prevConfig => ({
+                ...prevConfig,
+                category: {
+                    ...prevConfig.category,
+                    trivia: [...data.trivia_categories]
+                }
+            })
+        );
+        const getAPISettings = async () => {
+            // Need to request a Session KEY yet
+            try {
+                const categoryRes = await fetch('https://opentdb.com/api_category.php')
+                const responseData = await categoryRes.json();
+                return saveToGame(responseData)
+            } catch(err) {
+                console.log(err)
+            }
+        }
+        getAPISettings()
+    }, []);
 
-    const HomePage = (
+    const selectCategory = (e) => setMainConfig(prevConfig => ({
+            ...prevConfig,
+            category: {
+                ...prevConfig.category,
+                selected: e.target.value
+            }
+        })
+    );
+
+    const selectDifficulty = (e) => setMainConfig(prevConfig => ({
+            ...prevConfig,
+            difficulty: {
+                ...prevConfig.difficulty,
+                selected: e.target.value
+            }
+        })
+    );
+// console.log(mainConfig)
+    const devHomePage = (
         <React.Fragment>
-            <p className='home-text'>Let's Play a really cool Trivia Quiz!</p>
+            <p>Let's Play a really cool Trivia Quiz</p>
+            <form id='category-difficulty-form' className='quiz-form'>
+                <label>
+                    Pick a Category
+                    <select value={mainConfig.category.selected} onChange={selectCategory}>
+                        <option id={'default'} value={'any'}>Any category</option>
+                        {
+                            mainConfig.category.trivia.map((item, idx) => (
+                                <option key={item.id + idx} id={item.name} value={item.id}>
+                                    {item.name}
+                                </option>
+                            ))
+                        }
+                    </select>
+                </label>
+                <label>
+                    Difficulty
+                    <select value={mainConfig.difficulty.selected} onChange={selectDifficulty}>
+                        {
+                            mainConfig.difficulty.all.map((item, idx) => (
+                                <option key={idx + item} id={`level-${idx}`} value={item}>
+                                    {item}
+                                </option>
+                            ))
+                        }
+                    </select>
+                </label>
+            </form>
             <button
-                onClick={start}
                 className='start-btn'
+                onClick={start}
             >
                 Start Playing
             </button>
@@ -56,9 +107,11 @@ const App = () => {
             <Header />
             <main id='main' className='page-main'>
                 <div className='home'>
-                    {!gameStatus && HomePage}
+                    {!mainConfig.status && devHomePage}
                     <Quiz
-                        status={gameStatus}
+                        status={mainConfig.status}
+                        category={mainConfig.category.selected}
+                        level={mainConfig.difficulty.selected}
                         restartStatus={start}
                     />
                 </div>
