d6903d6d3aec0e36cb764c09ddc821b29e87eb71
jvitoralb <jvvalb@gmail.com>
2022-10-21 22:06:02 -0300

    last commit - edit

--- src/components/SubComponents/Questions.js
+++ src/components/SubComponents/Questions.js
@@ -4,15 +4,13 @@ import React from 'react';
 const Questions = ({ toRender, toRenderIndex, toRenderRef }) => {
 
     const getClass = () => {
-        if (toRenderRef.selected[1] === null) {
-            return '';
-        }
+        let classVal = '';
 
         if (toRenderRef.resolve === 'resolve') {
-            return toRenderRef.selected[1] ? 'q-right' : 'q-wrong';
+            classVal = (toRenderRef.selected[1] ? 'q-right' : 'q-wrong');
         }
 
-        return '';
+        return toRenderRef.selected[1] === null ? '' : classVal;
     }
 
     const decodedQuestion = decodeURIComponent(toRender.question);
