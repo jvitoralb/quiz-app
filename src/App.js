590d50c41c08e4e95464f3f79a5c2da200a396be
jvitoralb <jvvalb@gmail.com>
2022-11-05 10:42:55 -0300

    finish StartGamejs

--- src/App.js
+++ src/App.js
@@ -6,76 +6,40 @@ import './components/Styling/index.css';
 
 
 const App = () => {
-    const initialState = {
+    const initialAppConfig = {
         status: false,
-        category: {
-            trivia: [],
-            selected: 'any'
-        },
-        difficulty: {
-            all: ['any', 'easy', 'medium', 'hard'],
-            selected: 'any'
-        }
+        category: 'any',
+        difficulty: 'any'
     }
-    const [mainConfig, setMainConfig] = React.useState(initialState);
+    const [appConfig, setAppConfig] = React.useState(initialAppConfig);
 
-    const start = () => setMainConfig(prevConfig => ({
+    const start = () => setAppConfig(prevConfig => ({
             ...prevConfig,
             status: !prevConfig.status
         })
     );
 
-    React.useEffect(() => {
-        const saveToGame = (data) => setMainConfig(prevConfig => ({
-                ...prevConfig,
-                category: {
-                    ...prevConfig.category,
-                    trivia: [...data.trivia_categories]
-                }
-            })
-        );
-
-        const getAPISettings = async () => {
-            // Need to request a Session KEY yet
-            try {
-                const categoryRes = await fetch('https://opentdb.com/api_category.php');
-                const responseData = await categoryRes.json();
-                return saveToGame(responseData);
-            } catch(err) {
-                console.error(err)
-            }
-        }
-
-        getAPISettings()
-    }, []);
-
-    const selectCategory = (e) => setMainConfig(prevConfig => ({
+    const selectCategory = (e) => setAppConfig(prevConfig => ({
             ...prevConfig,
-            category: {
-                ...prevConfig.category,
-                selected: e.target.value
-            }
+            category: e.target.value
         })
     );
 
-    const selectDifficulty = (e) => setMainConfig(prevConfig => ({
+    const selectDifficulty = (e) => setAppConfig(prevConfig => ({
             ...prevConfig,
-            difficulty: {
-                ...prevConfig.difficulty,
-                selected: e.target.value
-            }
+            difficulty: e.target.value
         })
     );
 
     const settingCall = () => {
-        const { category, difficulty } = mainConfig;
+        const { category, difficulty } = appConfig;
         let categoryLevel = '';
 
-        if (category.selected !== 'any') {
-            categoryLevel = `&category=${category.selected}`;
+        if (category !== 'any') {
+            categoryLevel = `&category=${category}`;
         }
-        if (difficulty.selected !== 'any') {
-            categoryLevel = `${categoryLevel}&difficulty=${difficulty.selected}`;
+        if (difficulty !== 'any') {
+            categoryLevel = `${categoryLevel}&difficulty=${difficulty}`;
         }
 
         return categoryLevel;
@@ -88,11 +52,11 @@ const App = () => {
                 <div className='home'>
                     <StartGame
                         init={start}
-                        mainState={mainConfig}
+                        mainState={appConfig}
                         utility={{selectCategory, selectDifficulty}}
                     />
                     <Quiz
-                        status={mainConfig.status}
+                        status={appConfig.status}
                         settings={settingCall()}
                         restart={start}
                     />
--- src/components/StartGame.js
+++ src/components/StartGame.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './Styling/index.css';
 
 
@@ -5,10 +6,54 @@ const StartGame = ({ mainState, utility, init }) => {
     const { category, difficulty, status } = mainState;
     const { selectCategory, selectDifficulty } = utility;
 
+    const initialOpts = {
+        categories: [],
+        difficulties: [
+            'any', 'easy',
+            'medium', 'hard'
+        ]
+    }
+    const [ renderOpts, setRenderOpts ] = React.useState(initialOpts);
+
+    React.useEffect(() => {
+        const saveToState = (data) => setRenderOpts(prev => ({
+                ...prev,
+                categories: [
+                    ...data.trivia_categories
+                ]
+            })
+        );
+
+        const getAPISettings = async () => {
+            // Need to request a Session KEY
+            try {
+                const categoryRes = await fetch('https://opentdb.com/api_category.php');
+                const response = await categoryRes.json();
+                return saveToState(response);
+            } catch(err) {
+                console.error(err)
+            }
+        }
+
+        getAPISettings()
+    }, []);
+
     const selectChanges = (e) => {
         const { id } = e.target;
         return id === 'select-category' ? selectCategory(e) : selectDifficulty(e);
     }
+
+    const categoriesDrop = renderOpts.categories.map((item, idx) => (
+        <option key={item.id + idx} id={item.name} value={item.id}>
+            {item.name.replace('Entertainment: ', '')}
+        </option>
+    ));
+
+    const difficultiesDrop = renderOpts.difficulties.map((item, idx) => (
+        <option key={idx + item} id={`level-${idx}`} value={item}>
+            {item.slice(0, 1).toUpperCase() + item.slice(1)}
+        </option>
+    ));
 
     return (
         !status && <section id='welcome-section' className='home-sec'>
@@ -16,24 +61,16 @@ const StartGame = ({ mainState, utility, init }) => {
             <p>But first...</p>
             <form id='category-difficulty-form' className='quiz-form'>
                 <label>Pick a Category</label>
-                <select id='select-category' value={category.selected} onChange={selectChanges}>
+                <select id='select-category' value={category} onChange={selectChanges}>
                     <option id={'default'} value={'any'}>Any Category</option>
                     {
-                        category.trivia.map((item, idx) => (
-                            <option key={item.id + idx} id={item.name} value={item.id}>
-                                {item.name.replace('Entertainment: ', '')}
-                            </option>
-                        ))
+                        categoriesDrop
                     }
                 </select>
                 <label>Difficulty</label>
-                <select id='select-difficulty' value={difficulty.selected} onChange={selectChanges}>
+                <select id='select-difficulty' value={difficulty} onChange={selectChanges}>
                     {
-                        difficulty.all.map((item, idx) => (
-                            <option key={idx + item} id={`level-${idx}`} value={item}>
-                                {item.slice(0, 1).toUpperCase() + item.slice(1)}
-                            </option>
-                        ))
+                        difficultiesDrop
                     }
                 </select>
             </form>
